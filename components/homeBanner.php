07a2b6a35fc9000fc8243208c254e48a1b3fb72d
akeliansevag <akelian.sevag@gmail.com>
2025-01-15 10:34:12 +0200

    .

--- components/homeBanner.php
+++ components/homeBanner.php
@@ -1,14 +1,22 @@
 <section class="relative flex items-center min-h-[calc(100vh-92px)] bg-black w-full">
+    <a class="inline-block z-10 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" data-fancybox href="https://youtu.be/4D8S-mNlc8Y">
+        <svg width="100px" height="100px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M7.5 4.59863L18.6021 12L7.5 19.4014V4.59863ZM9 7.40141V16.5986L15.8979 12L9 7.40141Z" fill="#ffffff" />
+        </svg>
+    </a>
     <img class="absolute w-full h-full object-cover" src="<?= get_template_directory_uri() ?>/src/img/home-banner.webp" alt="The Speech banner">
     <div class="container">
+
         <div class="text-white relative lg:w-1/3">
-            <h1 class="max-lg:text-5xl text-[80px] leading-none">
+            <!-- <h1 class="max-lg:text-5xl text-[80px] leading-none">
                 <?= get_field('banner_title'); ?>
             </h1>
             <h2 class="max-lg:text-base text-[20px] uppercase mb-5">
                 <?= get_field('banner_subtitle'); ?>
-            </h2>
-            <a data-fancybox class="s-button border-primary bg-primary" href="https://youtu.be/4D8S-mNlc8Y"><?php _e('Watch Video', 'thespeech'); ?></a>
+            </h2> -->
+
+            <!-- <a data-fancybox class="s-button border-primary bg-primary" href="https://youtu.be/4D8S-mNlc8Y"><?php _e('Watch Video', 'thespeech'); ?></a> -->
+
         </div>
     </div>
 </section>
