9042f4f57dfeb6516881bab7977a596206be84af
akeliansevag <akelian.sevag@gmail.com>
2025-01-15 16:36:36 +0200

    .

--- components/CalendarSection.php
+++ components/CalendarSection.php
@@ -9,7 +9,7 @@ $args = array(
 );
 
 $posts = get_posts($args);
-
+dd($posts);
 ?>
 <?php if ($posts): ?>
     <section class="bg-[#021112] py-12 text-white">
@@ -23,7 +23,7 @@ $posts = get_posts($args);
                             <h4 class="text-[#707778] text-sm">January 21, 2025</h4>
 
                             <a href="<?= get_permalink($post->ID) ?>">
-                                <h3><?= $post->content; ?></h3>
+                                <h3><?= $post->post_content; ?></h3>
                             </a>
                             <div class="w-full h-[1px] bg-[#2C3636] mt-3"></div>
                         </div>
