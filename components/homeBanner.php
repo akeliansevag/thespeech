4574e74b2782e31990486d8595020c445bb3d764
akeliansevag <akelian.sevag@gmail.com>
2025-01-16 12:12:33 +0200

    .

--- components/homeBanner.php
+++ components/homeBanner.php
@@ -1,15 +1,15 @@
-<section class="relative flex max-lg:flex-col max-lg:justify-center items-center min-h-[calc(100vh-86px)] bg-black w-full">
+<section class="overflow-hidden relative flex max-lg:flex-col max-lg:justify-center items-center min-h-[calc(100vh-86px)] bg-black w-full">
 
-    <img class="absolute w-full h-full object-cover" src="<?= get_template_directory_uri() ?>/src/img/home-banner.webp" alt="The Speech banner">
+    <img class="image-wrap absolute w-full h-full object-cover" src="<?= get_template_directory_uri() ?>/src/img/microphone.webp" alt="The Speech banner">
     <div class="container">
 
-        <div class="text-white relative text-center max-w-[800px] mx-auto">
-            <a class="inline-block mb-3 z-10 relative" data-fancybox href="https://youtu.be/4D8S-mNlc8Y">
+        <div class="text-white relative max-w-[500px] text-left">
+            <!-- <a class="inline-block mb-3 z-10 relative" data-fancybox href="https://youtu.be/4D8S-mNlc8Y">
                 <svg width="100" height="100" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <circle cx="35" cy="35" r="34.5" stroke="#FFFFFF" />
                     <path d="M49.3174 34.6649L26.6469 47.7537L26.6469 21.5761L49.3174 34.6649Z" fill="#FFFFFF" />
                 </svg>
-            </a>
+            </a> -->
             <?php if (get_field('banner_title')): ?>
                 <h1 class="max-lg:text-5xl text-[80px] leading-[86px] font-[700]">
                     <?= get_field('banner_title'); ?>
@@ -21,7 +21,7 @@
                 </h2>
             <?php endif; ?>
 
-            <!-- <a data-fancybox class="s-button border-primary bg-primary" href="https://youtu.be/4D8S-mNlc8Y"><?php _e('Watch Video', 'thespeech'); ?></a> -->
+            <a data-fancybox class="s-button border-primary bg-primary" href="https://youtu.be/4D8S-mNlc8Y"><?php _e('Watch Video', 'thespeech'); ?></a>
 
         </div>
     </div>
