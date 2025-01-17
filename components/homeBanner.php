9ace05db49851bb29a5b543af79347811dba7eab
akeliansevag <akelian.sevag@gmail.com>
2025-01-17 10:28:53 +0200

    .

--- components/homeBanner.php
+++ components/homeBanner.php
@@ -3,28 +3,21 @@
     <img class="image-wrap absolute w-full h-full object-cover" src="<?= get_template_directory_uri() ?>/src/img/microphone.webp" alt="The Speech banner">
     <div class="container">
 
-        <div class="text-white relative max-w-[500px] ltr:text-left rtl:text-right">
-            <!-- <a class="inline-block mb-3 z-10 relative" data-fancybox href="https://youtu.be/4D8S-mNlc8Y">
-                <svg width="100" height="100" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <circle cx="35" cy="35" r="34.5" stroke="#FFFFFF" />
-                    <path d="M49.3174 34.6649L26.6469 47.7537L26.6469 21.5761L49.3174 34.6649Z" fill="#FFFFFF" />
-                </svg>
-            </a> -->
+        <div class="text-white relative max-w-[500px] flex flex-col gap-5 ltr:text-left rtl:text-right">
             <?php if (get_field('banner_title')): ?>
-                <h1 class=" max-lg:text-5xl text-[80px] leading-[86px]  mb-5 font-[700]">
+                <h1 class=" max-lg:text-5xl text-[80px] leading-[86px] font-[700]">
                     <?= get_field('banner_title'); ?>
                 </h1>
             <?php endif; ?>
             <?php if (get_field('banner_subtitle')): ?>
-                <h2 class="max-lg:text-base text-[20px] uppercase mb-5">
+                <h2 class="max-lg:text-base text-[20px] uppercase">
                     <?= get_field('banner_subtitle'); ?>
                 </h2>
             <?php endif; ?>
 
-            <div class="mt-7">
+            <div>
                 <a data-fancybox class="mt-10 s-button border-primary bg-primary" href="https://youtu.be/4D8S-mNlc8Y"><?php _e('Watch Video', 'thespeech'); ?></a>
             </div>
-
 
         </div>
     </div>
